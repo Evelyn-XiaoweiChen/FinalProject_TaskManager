9bf518d95d3311faaf0e0f779d58f260ff1446e7
Xiaowei Chen <xiaoqianniu@gmail.com>
2023-07-09 21:43:43 -0400

    version5: signIn firebase authentication is ready

--- app/src/main/java/ca/xiaowei/chen2267127/LoginActivity.java
+++ app/src/main/java/ca/xiaowei/chen2267127/LoginActivity.java
@@ -1,18 +1,41 @@
 package ca.xiaowei.chen2267127;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 TextView newUserText;
 EditText emailText,passwordText;
 Button signInBtn;
+String emailValue,passwordValue;
+FirebaseAuth mAuth;
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null)
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +43,7 @@ Button signInBtn;
         initialize();
     }
     public void initialize(){
+        mAuth = FirebaseAuth.getInstance();
         newUserText = findViewById(R.id.newUserText);
         newUserText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,10 +61,33 @@ Button signInBtn;
 
     @Override
     public void onClick(View v) {
+        emailValue = emailText.getText().toString();
+        passwordValue = passwordText.getText().toString();
+        if(TextUtils.isEmpty(emailValue)){
+            Toast.makeText(this,"Enter email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(passwordValue)){
+            Toast.makeText(this,"Enter password",Toast.LENGTH_SHORT).show();
+            return;
+        }
         if(v.getId() == R.id.signInButton){
-            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-            startActivity(intent);
-            finish();
+            mAuth.signInWithEmailAndPassword(emailValue, passwordValue)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
         }
 
     }
