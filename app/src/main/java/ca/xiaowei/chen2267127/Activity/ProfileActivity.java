c5d6c5a512bf8f88337dba2013168b20ba80e866
Xiaowei Chen <xiaoqianniu@gmail.com>
2023-07-12 10:55:15 -0400

    version11: ProfileActivity, updatapassword is ready, and synchronous the updated psw in firestore too

--- app/src/main/java/ca/xiaowei/chen2267127/Activity/ProfileActivity.java
+++ app/src/main/java/ca/xiaowei/chen2267127/Activity/ProfileActivity.java
@@ -6,32 +6,41 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import ca.xiaowei.chen2267127.R;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
     DrawerLayout drawerLayout;
     ImageView menu;
     LinearLayout home, profile, logout;
     FirebaseAuth mAuth;
-    TextView usernameText,emailText;
+    TextView usernameText, emailText;
+    Button editProfileBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +49,12 @@ public class ProfileActivity extends AppCompatActivity {
         initialize();
         fetchDataFromFirestore();
     }
+
     private void initializeFirebaseAuth() {
         mAuth = FirebaseAuth.getInstance();
     }
-    public void initialize(){
+
+    public void initialize() {
         drawerLayout = findViewById(R.id.drawerLayout);
         usernameText = findViewById(R.id.profileUsername);
         emailText = findViewById(R.id.profileEmail);
@@ -51,6 +62,8 @@ public class ProfileActivity extends AppCompatActivity {
         home = findViewById(R.id.home);
         profile = findViewById(R.id.profile);
         logout = findViewById(R.id.exitApp);
+        editProfileBtn = findViewById(R.id.editProfileButton);
+        editProfileBtn.setOnClickListener(this);
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,7 +90,7 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
-    public void fetchDataFromFirestore(){
+    public void fetchDataFromFirestore() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         String userId = mAuth.getCurrentUser().getUid();
         db.collection("users")
@@ -98,7 +111,7 @@ public class ProfileActivity extends AppCompatActivity {
                                 usernameText.setText(username);
                                 emailText.setText(email);
                             } else {
-                               // Toast.makeText(this,"user does not exist",Toast.LENGTH_SHORT).show();
+                                // Toast.makeText(this,"user does not exist",Toast.LENGTH_SHORT).show();
                             }
                         } else {
                             // Error fetching data from Firestore
@@ -106,6 +119,7 @@ public class ProfileActivity extends AppCompatActivity {
                     }
                 });
     }
+
     public static void openDrawer(DrawerLayout drawerLayout) {
         drawerLayout.openDrawer(GravityCompat.START);
     }
@@ -129,4 +143,128 @@ public class ProfileActivity extends AppCompatActivity {
         closeDrawer(drawerLayout);
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.editProfileButton) {
+            showUpdatePasswordDialog();
+        }
+    }
+
+    private void showUpdatePasswordDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.update_password_dialog, null);
+
+        EditText currentPasswordEditText = dialogView.findViewById(R.id.textCurrentPassword);
+        EditText newPasswordEditText = dialogView.findViewById(R.id.textNewPassword);
+        EditText confirmNewPasswordEditText = dialogView.findViewById(R.id.textConfirmNewPassword);
+
+        builder.setView(dialogView)
+                .setTitle("Update Password")
+                .setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String currentPassword = currentPasswordEditText.getText().toString();
+                        String newPassword = newPasswordEditText.getText().toString();
+                        String confirmNewPassword = confirmNewPasswordEditText.getText().toString();
+
+                        // Perform password update logic here
+                        // Call the method to update the password
+                        updatePassword(currentPassword, newPassword, confirmNewPassword);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private void updatePassword(String currentPassword, String newPassword, String confirmNewPassword) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            // Check if the new password and confirm password match
+            if (!newPassword.equals(confirmNewPassword)) {
+                Toast.makeText(this, "New password and confirm password do not match.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Reauthenticate the user with their current password
+            AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), currentPassword);
+            user.reauthenticate(credential)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                // Reauthentication successful, update the password
+                                user.updatePassword(newPassword)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    // Password updated successfully
+                                                    Toast.makeText(ProfileActivity.this, "Password updated successfully.", Toast.LENGTH_SHORT).show();
+
+                                                    //keep in mind that the password field in Firestore is not automatically synchronized with the updated password.
+                                                    // need to explicitly update the password field in Firestore after a successful password update.
+                                                    updatePasswordInFirestore(newPassword);
+                                                    // Send email notification to the user
+                                                    sendPasswordChangeEmail();
+                                                } else {
+                                                    // Failed to update password
+                                                    Toast.makeText(ProfileActivity.this, "Failed to update password.", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+
+
+                                        });
+                            } else {
+                                // Reauthentication failed
+                                Toast.makeText(ProfileActivity.this, "Failed to authenticate with current password.", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    // synchronous firestore after updating psw
+    private void updatePasswordInFirestore(String newPassword) {
+        String userId = mAuth.getCurrentUser().getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference userRef = db.collection("users").document(userId);
+        Map<String, Object> updatedData = new HashMap<>();
+        updatedData.put("password", newPassword);
+
+        userRef.update(updatedData)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Password field updated in Firestore
+                            Toast.makeText(ProfileActivity.this, "Password updated in Firestore.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Failed to update password field in Firestore
+                            Toast.makeText(ProfileActivity.this, "Failed to update password in Firestore.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendPasswordChangeEmail() {
+        String email = mAuth.getCurrentUser().getEmail();
+
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Email sent successfully
+                            Toast.makeText(ProfileActivity.this, "Password change email sent.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Email failed to send
+                            Toast.makeText(ProfileActivity.this, "Failed to send password change email.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
 }
